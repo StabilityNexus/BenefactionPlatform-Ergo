20cb40043480b72ff4023d5581338ca48952882e
0xf965 <thisemaildoesnotexists@anon.erg>
2024-10-29 09:21:25 +0100

    improved exchange script

--- src/lib/ergo/exchange.ts
+++ src/lib/ergo/exchange.ts
@@ -18,54 +18,37 @@ export async function exchange(
     project: Project,
     token_amount: number
 ): Promise<string|null> {
-    
-    console.log("wants to buy ", token_amount)
 
     let ergo_amount = token_amount * project.exchange_rate
 
-    // Get the wallet address (will be the project address)
+    // Get the wallet address (will be the user address)
     const walletPk = await ergo.get_change_address();
     
     // Get the UTXOs from the current wallet to use as inputs
     const inputs = [project.box, ...(await ergo.get_utxos())];
 
-    // Building the project output
-    let outputs: OutputBuilder[] = [];
-    const contractOutput = new OutputBuilder(
-        project.value + ergo_amount,
-        ergo_tree_address    // Address of the project contract
-    );
-
     const devAddress = "0xabcdefghijklmnñoqrstuvwxyz";
     const devFeePercentage = 5;
 
-    contractOutput.addTokens({
-        tokenId: project.token_id,
-        amount: (project.total_amount - token_amount).toString()
-    }, {sum: true});
-
-    // Set additional registers in the output box
-    contractOutput.setAdditionalRegisters({
-        R4: SInt(project.block_limit).toHex(),                                   // Block limit for withdrawals/refunds
-        R5: SLong(BigInt(project.minimum_amount)).toHex(),                       // Minimum sold
-        R6: SLong(BigInt(project.amount_sold + token_amount)).toHex(),           // Tokens sold counter
-        R7: SLong(BigInt(project.exchange_rate)).toHex(),                        // Exchange rate ERG/Token
-        R8: SString(await sha256(walletPk)),                          // Withdrawal address (hash of walletPk)
-        R9: SString(project.link)                                     // Link or hash with project info
-    });
-    outputs.push(contractOutput);
-
-    const userOutput = new OutputBuilder(
-        BigInt(Number(get(balance)) - ergo_amount),
-        get(address) ?? ""
-    )
-    .addTokens({
-        tokenId: project.token_id,
-        amount: token_amount.toString()
-    }, {sum: true});
-    
-    outputs.push(userOutput);
-    
+    // Building the project output
+    let outputs: OutputBuilder[] = [
+        new OutputBuilder(
+            BigInt(project.value + ergo_amount).toString(),
+            ergo_tree_address    // Address of the project contract
+        )
+        .addTokens({
+            tokenId: project.token_id,
+            amount: (project.total_amount - token_amount).toString()
+        })
+        .setAdditionalRegisters({
+            R4: SInt(project.block_limit).toHex(),                                   // Block limit for withdrawals/refunds
+            R5: SLong(BigInt(project.minimum_amount)).toHex(),                       // Minimum sold
+            R6: SLong(BigInt(project.amount_sold + token_amount)).toHex(),           // Tokens sold counter
+            R7: SLong(BigInt(project.exchange_rate)).toHex(),                        // Exchange rate ERG/Token
+            R8: SString(project.owner),                                     // Withdrawal address (hash of walletPk)
+            R9: SString(project.link)                                                // Link or hash with project info
+        })
+    ];    
 
     // Building the unsigned transaction
     const unsignedTransaction = await new TransactionBuilder(await ergo.get_current_height())
@@ -75,7 +58,7 @@ export async function exchange(
         .payFee(RECOMMENDED_MIN_FEE_VALUE)     // Pay the recommended minimum fee
         .build()                               // Build the transaction
         .toEIP12Object();                      // Convert the transaction to an EIP-12 compatible object
-
+    
     // Sign the transaction
     const signedTransaction = await ergo.sign_tx(unsignedTransaction);
 
