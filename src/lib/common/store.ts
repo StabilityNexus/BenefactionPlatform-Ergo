dec01d0cae7956974075d5c654e6f903885c865a
0xf965 <thisemaildoesnotexists@anon.erg>
2024-11-12 16:54:09 +0100

    Updates

--- src/lib/common/store.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Project } from './project';
-
-export const address = writable<string|null>(null);
-export const network = writable<string|null>(null);
-export const connected = writable<boolean>(false);
-export const balance = writable<number|null>(null);
-export const project_detail = writable<Project|null>(null);
