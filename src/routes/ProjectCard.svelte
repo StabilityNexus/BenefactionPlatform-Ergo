f9b5712703eb3bac94d28f81367c0bf7828c6a74
0xf965 <thisemaildoesnotexists@anon.erg>
2024-12-04 19:11:41 +0100

    image on project card.

--- src/routes/ProjectCard.svelte
+++ src/routes/ProjectCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { block_to_time } from "$lib/common/countdown";
     import { is_ended, min_raised, type Project } from "$lib/common/project";
-    import {project_detail} from "$lib/common/store";
+    import { project_detail } from "$lib/common/store";
     import { Button } from "spaper";
 
     // Define 'project' as a prop of type Project
@@ -10,13 +10,14 @@
     let deadline_passed = false;
     let is_min_raised = false;
     let limit_date = "";
-    async function load()
-    {
+    async function load() {
         deadline_passed = await is_ended(project);
-        is_min_raised = await min_raised(project)
-        limit_date = new Date(await block_to_time(project.block_limit, project.platform)).toLocaleString();
+        is_min_raised = await min_raised(project);
+        limit_date = new Date(
+            await block_to_time(project.block_limit, project.platform)
+        ).toLocaleString();
     }
-    load()
+    load();
 
     function toggleDetails() {
         // Get the current value of the store
@@ -24,30 +25,43 @@
     }
 </script>
 
-<div class="card">
+<div
+    class="card"
+    style="background-image: url({project.content.image}); background-size: cover; background-position: center; background-repeat: no-repeat;"
+>
     <div class="card-body">
         <h3 class="card-title">{project.content.title}</h3>
-        <p>{project.content.description.length > 48 ? project.content.description.slice(0, 48) + " ...." : project.content.description}</p>
+        <p>
+            {project.content.description.length > 48
+                ? project.content.description.slice(0, 48) + " ...."
+                : project.content.description}
+        </p>
         <p><strong>Limit date:</strong> {limit_date}</p>
         <!-- <p><strong>ERGs collected (included refuned or withdraw):</strong> {project.collected_value/1000000000} ERG</p>  -->
-        <p><strong>Current ERG balance:</strong> {project.current_value/1000000000} ERG</p>
-        <p><strong>Deadline passed:</strong> {deadline_passed ? "Yes": "No"}</p>
-        <p><strong>Min value raised:</strong> {is_min_raised ? "Yes": "No"}</p>
-        <Button on:click={toggleDetails} style="background-color: orange; color: black; border: none; padding: 0.25rem 1rem; font-size: 1rem;">View</Button>
-        
+        <p><strong>Current ERG balance:</strong> {project.current_value / 1000000000} ERG</p>
+        <p><strong>Deadline passed:</strong> {deadline_passed ? "Yes" : "No"}</p>
+        <p><strong>Min value raised:</strong> {is_min_raised ? "Yes" : "No"}</p>
+        <Button
+            on:click={toggleDetails}
+            style="background-color: orange; color: black; border: none; padding: 0.25rem 1rem; font-size: 1rem;"
+        >
+            View
+        </Button>
     </div>
 </div>
 
 <style>
-    /* Custom styles for the card */
     .card {
         margin: 1rem 0;
         border: 1px solid #ddd;
         color: #ddd;
+        position: relative;
     }
 
     .card-body {
         padding: 1rem;
+        background-color: rgba(0, 0, 0, 0.8);
+        color: #fff;
     }
 
     .card-title {
