d40c0be29307905ec014235e66d18b499cebff54
0xf965 <thisemaildoesnotexists@anon.erg>
2024-12-09 23:29:17 +0100

    ergo mainnet

--- src/lib/ergo/envs.ts
+++ src/lib/ergo/envs.ts
@@ -1,4 +1,4 @@
-export const network_id: "mainnet"|"testnet" = "testnet";
+export const network_id: "mainnet"|"testnet" = "mainnet";
 export const explorer_uri =          (network_id == "mainnet") ? "https://api.ergoplatform.com"          : "https://api-testnet.ergoplatform.com";
 export const web_explorer_uri_tx =   (network_id == "mainnet") ? "https://sigmaspace.io/en/transaction/" : "https://testnet.ergoplatform.com/transactions/";
 export const web_explorer_uri_addr = (network_id == "mainnet") ? "https://sigmaspace.io/en/address/"     : "https://testnet.ergoplatform.com/addresses/";
